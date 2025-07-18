b99d96c14519b4d983a8804e9f257d07b8310354
igortxra <igorxteixeira@gmail.com>
2025-07-18 18:59:43 -0300

    Second Version
    sei la o que eu fiz aqui
    
    API base
    
    : 2.5ms p99

---
+++ NOTAS.md
@@ -0,0 +1,6 @@
+POSSIBILIDADE
+- Desabilitar logs o Nginx ajuda no ganho de performance não ajuda muito
+
+TRUE
+
+FALSE
--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,40 +1,84 @@
+from logging import Logger
+from fastapi import FastAPI, Query, HTTPException
+from datetime import datetime, timezone
 import asyncio
+import httpx
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Query
-
-from .workers import payment_worker, update_best_processor
-from .api import create_payment, get_payments_summary
 
 
+# ADICIONAR UM BANCO DE DADOS PARA ENVIAR OS PAGAMETNOS
+# COLD START DELAY PARA DAR TEMPO DE BAIXAR CPU DOS BACKENDS
 
-NUM_WORKERS = 100
 
+# VARIAVEIS DE AMBIENTE NO FUTURO
+BATCH_SIZE = 200
+TARGET_URL = "http://payment-processor-default:8080"
+
+
+payment_queue = asyncio.Queue()
+
+# === Envia 1 pagamento e, se falhar, reenvia ===
+async def send_payment(client: httpx.AsyncClient , payment):
+    response = await client.post(f"{TARGET_URL}/payments", json=payment)
+    if response.status_code != 200:
+        await payment_queue.put(payment)
+        await asyncio.sleep(1)
+
+    if response.elapsed.total_seconds() >= 1:
+        await asyncio.sleep(1)
+    
+
+# === Worker que processa lotes de 100 pagamentos ===
+async def payment_worker():
+    # async with httpx.AsyncClient() as client:
+    while True:
+        try:
+            payment = await payment_queue.get_nowait()
+            payment_queue.task_done()
+        except Exception:
+            continue
+            
+
+            # batch = []
+            # while len(batch) < BATCH_SIZE:
+            #     item = await payment_queue.get()
+            #     batch.append(item)
+            # 
+            # # Envia as 100 requisições em paralelo com tratamento de erro
+            # await asyncio.gather(*[
+            #     send_payment(client, payment) for payment in batch
+            # ])
+
+# === FastAPI lifespan: inicia o worker no startup ===
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    tasks = [asyncio.create_task(payment_worker()) for _ in range(NUM_WORKERS)]
-    tasks.append(asyncio.create_task(update_best_processor()))
+    asyncio.create_task(payment_worker())
     yield
-    for task in tasks:
-        task.cancel()
-    await asyncio.gather(*tasks, return_exceptions=True)
-
-app = FastAPI(
-    title="Rinha Backend API", 
-    version="1.0.0",
-    lifespan=lifespan
-)
 
 
+app = FastAPI(title="Rinha Backend API", version="1.0.0")
+# app.router.lifespan_context = lifespan
+
+
+# ROTAS
 @app.post("/payments", status_code=200)
 async def payments_endpoint(payment: dict):
-    return await create_payment(payment)
+    payment_queue.put_nowait(payment)
 
 
 @app.get("/payments-summary")
-async def payments_summary_endpoint(from_: str = Query(None, alias="from"), to: str = None):
-    return await get_payments_summary(from_, to)
+async def payments_summary_endpoint(
+    from_datetime: str = Query(None, alias="from"), 
+    to_datetime: str = Query(None, alias="to"),
+):
+    return {
+        "default" : {
+            "totalRequests": 0,
+            "totalAmount": round(0 * 19.9, 2),
+        },
+        "fallback" : {
+            "totalRequests": 0,
+            "totalAmount": round(0 * 19.9, 2),
+        }
+    }
 
-
-@app.get("/health")
-async def health_endpoint():
-    return {"status": "healthy"}
