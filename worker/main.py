6d8f7afbaed231414dcd4b622103013bdd103237
igortxra <igorxteixeira@gmail.com>
2025-08-01 19:25:21 -0300

    feito

--- worker/main.py
+++ worker/main.py
@@ -10,15 +10,18 @@ import asyncio
 STREAM_NAME = "payments"
 
 PROCESSOR_URL = {
-    "default": os.getenv("PAYMENT_PROCESSOR_URL_DEFAULT", "http://payment-processor-default:8080"),
-    "fallback": os.getenv("PAYMENT_PROCESSOR_URL_FALLBACK", "http://payment-processor-fallback:8080")
+    "default": os.getenv(
+        "PAYMENT_PROCESSOR_URL_DEFAULT", "http://payment-processor-default:8080"
+    ),
+    "fallback": os.getenv(
+        "PAYMENT_PROCESSOR_URL_FALLBACK", "http://payment-processor-fallback:8080"
+    ),
 }
 
 HEALTH_STATUS = {
     "default": {"failing": False, "minResponseTime": 0},
-    "fallback": {"failing": False, "minResponseTime": 0}
+    "fallback": {"failing": False, "minResponseTime": 0},
 }
-
 
 DEFAULT_WORKERS = 25
 FALLBACK_WORKERS = 20
@@ -33,21 +36,22 @@ CONTINGENCY = {
     "fallback": False,
 }
 
-RECOVERY_MODE = {
-    "default": False,
-    "fallback": False
-}
+RECOVERY_MODE = {"default": False, "fallback": False}
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379")
 
 REDIS = Redis.from_url(REDIS_URL, decode_responses=True)
 
 
-async def process_payment(msg_id, payload, client: httpx.AsyncClient, processor_name: str) -> Tuple[bool, float]:
+async def process_payment(
+    msg_id, payload, client: httpx.AsyncClient, processor_name: str
+) -> Tuple[bool, float]:
     requested_at = datetime.now(timezone.utc)
-    payload['requestedAt'] =  requested_at.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+    payload["requestedAt"] = requested_at.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
     score = requested_at.timestamp()
-    response = await client.post(f"{PROCESSOR_URL[processor_name]}/payments", json=payload, timeout=10)
+    response = await client.post(
+        f"{PROCESSOR_URL[processor_name]}/payments", json=payload, timeout=10
+    )
     if response.status_code == 200:
         await REDIS.zadd(f"{STREAM_NAME}:{processor_name}", {msg_id: score})
         await REDIS.xack(STREAM_NAME, "payment-workers", msg_id)
@@ -56,18 +60,18 @@ async def process_payment(msg_id, payload, client: httpx.AsyncClient, processor_
     RECOVERY_MODE[processor_name] = True
     HEALTH_STATUS[processor_name]["failing"] = True
     return False, 0
-    
+
 
 async def contingency_worker(processor_name: str):
     CONTINGENCY[processor_name] = True
     async with httpx.AsyncClient() as client:
         while HEALTH_STATUS[processor_name]["failing"]:
             msgs = await REDIS.xreadgroup(
-                groupname='payment-workers',
+                groupname="payment-workers",
                 consumername=f"{processor_name}-contingency",
-                streams={STREAM_NAME: '>'},
+                streams={STREAM_NAME: ">"},
                 count=1,
-                block=5000  # espera até 100ms por nova mensagem
+                block=5000,  # espera até 100ms por nova mensagem
             )
 
             if not msgs:
@@ -80,7 +84,9 @@ async def contingency_worker(processor_name: str):
                 for msg_id, payload in entries:
                     message_processed = False
                     while not message_processed:
-                        response = await process_payment(msg_id, payload, client, processor_name)
+                        response = await process_payment(
+                            msg_id, payload, client, processor_name
+                        )
                         success, minResponseTime = response
 
                         # Verifica se a falha foi resolvida
@@ -89,7 +95,9 @@ async def contingency_worker(processor_name: str):
                             if minResponseTime <= 100:
                                 RECOVERY_MODE[processor_name] = False
                                 HEALTH_STATUS[processor_name]["failing"] = False
-                                HEALTH_STATUS[processor_name]["minResponseTime"] = minResponseTime
+                                HEALTH_STATUS[processor_name]["minResponseTime"] = (
+                                    minResponseTime
+                                )
                                 return
 
                         await asyncio.sleep(0.005)
@@ -100,7 +108,6 @@ async def contingency_worker(processor_name: str):
 
 
 async def worker(processor_name: str, consumer_name: str):
-
     limits = httpx.Limits(max_connections=1000, max_keepalive_connections=32)
     async with httpx.AsyncClient(limits=limits) as client:
         while True:
@@ -120,13 +127,21 @@ async def worker(processor_name: str, consumer_name: str):
                 if HEALTH_STATUS[processor_name]["minResponseTime"] > 100:
                     await asyncio.sleep(0.005)
                     continue
-            
-            stream_batch = await REDIS.xreadgroup(groupname='payment-workers', consumername=consumer_name, streams={STREAM_NAME: '>'}, count=batch, block=0)
-            
+
+            stream_batch = await REDIS.xreadgroup(
+                groupname="payment-workers",
+                consumername=consumer_name,
+                streams={STREAM_NAME: ">"},
+                count=batch,
+                block=0,
+            )
+
             tasks = []
             for _, entries in stream_batch:
                 for msg_id, payload in entries:
-                    tasks.append(process_payment(msg_id, payload, client, processor_name))
+                    tasks.append(
+                        process_payment(msg_id, payload, client, processor_name)
+                    )
 
             if not tasks:
                 await asyncio.sleep(0.005)
@@ -135,34 +150,45 @@ async def worker(processor_name: str, consumer_name: str):
             await asyncio.gather(*tasks)
 
 
-async def health_check(processor_name: str, delay: float = 0, interval = 5):
+async def health_check(processor_name: str, delay: float = 0, interval=5):
     await asyncio.sleep(delay)
     async with httpx.AsyncClient() as client:
         while True:
-            response = await client.get(PROCESSOR_URL[processor_name] + "/payments/service-health")
+            response = await client.get(
+                PROCESSOR_URL[processor_name] + "/payments/service-health"
+            )
             if response.status_code == 200:
                 new_status = response.json()
                 HEALTH_STATUS[processor_name] = new_status
                 if new_status["failing"]:
                     RECOVERY_MODE[processor_name] = True
             else:
-                HEALTH_STATUS[processor_name] = {"failing": True, "minResponseTime": HEALTH_STATUS[processor_name]["minResponseTime"]}
+                HEALTH_STATUS[processor_name] = {
+                    "failing": True,
+                    "minResponseTime": HEALTH_STATUS[processor_name]["minResponseTime"],
+                }
 
             await asyncio.sleep(interval)
 
 
 async def main():
-
     try:
-        await REDIS.xgroup_create(name=STREAM_NAME, groupname="payment-workers", id="0", mkstream=True)
+        await REDIS.xgroup_create(
+            name=STREAM_NAME, groupname="payment-workers", id="0", mkstream=True
+        )
     except Exception:
         # Group already created
         pass
 
     default_workers = [worker("default", f"d{i}") for i in range(DEFAULT_WORKERS)]
     fallback_workers = [worker("fallback", f"f{i}") for i in range(FALLBACK_WORKERS)]
-    
-    workers = [*default_workers, *fallback_workers, health_check("default", 0), health_check("fallback", 2.5)]
+
+    workers = [
+        *default_workers,
+        *fallback_workers,
+        health_check("default", 0),
+        health_check("fallback", 2.5),
+    ]
 
     await asyncio.gather(*workers)
 
